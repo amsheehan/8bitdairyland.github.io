ed04f8729b5a3cdd6a6578f97c35c741ca833b8c
amsheehan <alex.m.sheehan@gmail.com>
2014-03-12 02:42:24 -0400

    added some landing design

--- _site/js/page-composition.js
+++ _site/js/page-composition.js
@@ -2,9 +2,11 @@ $(document).ready(function(){
   //var imgHeight = $('.header-img').height();
   
   var windowHeight = window.innerHeight;
+  // Add and subtract 20 because of the level's ground
   var canvasHeight = windowHeight - 20;
+  var contentMargin = canvasHeight + 20;
 
   $('.header-lvl').css("height", windowHeight + "px");
-  //$('.header-overlay').css("height", imgHeight + "px");
   $('#level-canvas').css("height", canvasHeight + "px");
+  $('.content-overlay').css("top", contentMargin + "px");
 });
